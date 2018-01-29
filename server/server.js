ea1c55ad2555af49d93586f1e718c8a1e3fe7439
Steve Sargent <ssarge70@gmail.com>
2018-01-28 18:59:01 -0800

    Initial Commit w/server.js and index.html

---
+++ server/server.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const path = require('path')
+const publicPath = path.join(__dirname, '../public');
+const express = require('express');
+const port = process.env.PORT || 3000;
+let app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
+});
