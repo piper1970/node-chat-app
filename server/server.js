85ea01dce92f42c676346c3e8bceb17e3835d783
Steve Sargent <ssarge70@gmail.com>
2018-01-30 06:32:24 -0800

    Add sockets connect and disconnect

--- server/server.js
+++ server/server.js
@@ -1,13 +1,26 @@
 'use strict'
 
-const path = require('path')
-const publicPath = path.join(__dirname, '../public');
+const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
+
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
-let app = express();
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('Disconnected from client');
+  });
+});
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
