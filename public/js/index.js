e3655640175f5c39f15cf1b2e5bae1596468e08e
Steve Sargent <ssarge70@gmail.com>
2018-02-06 18:31:23 -0800

    Add mustache.js for templating

--- public/js/index.js
+++ public/js/index.js
@@ -11,23 +11,27 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
+
   var formattedTime = moment(message.createdAt).format('h:mm a');
-  console.log('newMessage', message);
-  var li = $('<li></li>');
-  li.text(message.from + " " + formattedTime + ": " + message.text);
-  $('#messages').append(li);
+  var template = $('#message-template').html();
+  var html = Mustache.render(template, {
+    text: message.text,
+    createdAt: formattedTime,
+    from: message.from
+  });
+  jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function(msg) {
-  var li = $('<li></li>');
-  var a = $('<a target="_blank">My current location</a>');
+
   var formattedTime = moment(msg.createdAt).format('h:mm a');
-
-  li.text(msg.from + ' ' + formattedTime + ': ');
-  a.attr('href', msg.url);
-
-  li.append(a);
-  $('#messages').append(li);
+  var template = $('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: msg.from,
+    createdAt: formattedTime,
+    url:msg.url
+  });
+  $('#messages').append(html);
 });
 
 $('#message-form').on('submit', function(e){
